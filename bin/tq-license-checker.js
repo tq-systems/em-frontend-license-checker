d5e30be7e2f5315c6d8c9c50c4c7ec16feb8bee7
Daniel Schmidt <Daniel.Schmidt@tq-group.com>
2019-01-23 08:11:28 +0100

    Remove incompatible licenses.

--- bin/tq-license-checker.js
+++ bin/tq-license-checker.js
@@ -4,10 +4,9 @@ var licenseWhitelist = "";
 // official SPDX identifiers
 licenseWhitelist += "Apache-2.0;";
 licenseWhitelist += "BSD-2-Clause;BSD-3-Clause;";
-licenseWhitelist += "CC-BY-3.0;CC-BY-4.0;CC0-1.0;";
+licenseWhitelist += "CC0-1.0;";
 licenseWhitelist += "ISC;";
 licenseWhitelist += "MIT;";
-licenseWhitelist += "OFL-1.1;";
 licenseWhitelist += "Unlicense;";
 licenseWhitelist += "WTFPL;";
 licenseWhitelist += "Zlib;";
