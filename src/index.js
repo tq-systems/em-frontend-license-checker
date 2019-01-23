e61f1d99759ffdf062082105d6a57e91c2d44d8c
Daniel Schmidt <Daniel.Schmidt@tq-group.com>
2019-01-23 10:16:03 +0100

    Add export of allowed licenses.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const AllowedLicenses = {
+    spdx : "(Apache-2.0 OR BSD-2-Clause OR BSD-3-Clause OR CC0-1.0 OR ISC OR MIT OR Unlicense OR WTFPL OR Zlib OR AFL-2.1)"
+}
+
+export default AllowedLicenses
