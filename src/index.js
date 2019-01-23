9880c2eb535b8dc598c23d185a74e5815ca95361
Daniel Schmidt <Daniel.Schmidt@tq-group.com>
2019-01-23 10:20:28 +0100

    Fix module export.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const AllowedLicenses = {
     spdx : "(Apache-2.0 OR BSD-2-Clause OR BSD-3-Clause OR CC0-1.0 OR ISC OR MIT OR Unlicense OR WTFPL OR Zlib OR AFL-2.1)"
 }
 
-export default AllowedLicenses
+module.exports = AllowedLicenses;
